4087c4983f128fefa9415db9c51d26c6a782e6f8
Swarnim Kashyap <68059131+Swarnim2511@users.noreply.github.com>
2023-07-10 15:40:25 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Welcome to FiftyFive \n";
+        return "Welcome to FiftyFive Technologies \n";
     }
     
 }
